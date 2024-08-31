e04c4128c0ee3b3e1aff0cd9935b19d3a5b526e2
Vikram Mogaveera <vikram.mogaveera@hcl.com>
2024-08-31 16:06:44 +0530

    committing from master

--- src/main/java/com/mycompany/app/Test.java
+++ src/main/java/com/mycompany/app/Test.java
@@ -3,5 +3,6 @@ package com.mycompany.app;
 public class Test {
     public static void main(String[] args) {
         System.out.println("This is test file for new branch");
+        System.out.println("Committing from master");
     }
 }
