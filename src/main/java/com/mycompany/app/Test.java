182cf35e0516bb41eb0b83f2399a124e8978b500
VikramMogaveera <65918826+VikramMogaveera@users.noreply.github.com>
2024-08-31 20:45:26 +0530

    Update Test.java

--- src/main/java/com/mycompany/app/Test.java
+++ src/main/java/com/mycompany/app/Test.java
@@ -3,10 +3,7 @@ package com.mycompany.app;
 public class Test {
     public static void main(String[] args) {
         System.out.println("This is test file for new branch");
-<<<<<<< HEAD
-=======
         System.out.println("Committing from master");
         System.out.println("This branch is not updated");
->>>>>>> ad1f5f355fd5f4a960f0b642c688b4c434b21be4
     }
 }
