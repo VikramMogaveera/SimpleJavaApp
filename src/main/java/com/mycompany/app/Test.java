28d4ef5420ae54d0b871d9cb2ea90672de69019e
Vikram Mogaveera <vikram.mogaveera@hcl.com>
2024-08-31 19:46:23 +0530

    Revert "committing from master"
    This reverts commit e04c4128c0ee3b3e1aff0cd9935b19d3a5b526e2.

--- src/main/java/com/mycompany/app/Test.java
+++ src/main/java/com/mycompany/app/Test.java
@@ -3,6 +3,5 @@ package com.mycompany.app;
 public class Test {
     public static void main(String[] args) {
         System.out.println("This is test file for new branch");
-        System.out.println("Committing from master");
     }
 }
