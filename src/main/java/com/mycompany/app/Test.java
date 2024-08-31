8d148577ee968a601fa0f1e0e9a04bb9a2370e4d
Vikram Mogaveera <vikram.mogaveera@hcl.com>
2024-08-31 16:56:47 +0530

    Resolving the merge conflict after stash the changes

--- src/main/java/com/mycompany/app/Test.java
+++ src/main/java/com/mycompany/app/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("This is test file for new branch");
         System.out.println("Committing from master");
+        System.out.println("This branch is not updated");
     }
 }
